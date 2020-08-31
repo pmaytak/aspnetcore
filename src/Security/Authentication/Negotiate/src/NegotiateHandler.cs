43ec2c3c96552857b1d8c167b4ab02f2775d4ec4
Pranav K <prkrishn@hotmail.com>
2020-08-31 08:11:53 -0700

    Fixup

--- src/Security/Authentication/Negotiate/src/NegotiateHandler.cs
+++ src/Security/Authentication/Negotiate/src/NegotiateHandler.cs
@@ -315,11 +315,9 @@ namespace Microsoft.AspNetCore.Authentication.Negotiate
             // things like ClaimsTransformation run per request.
             var identity = _negotiateState.GetIdentity();
             ClaimsPrincipal user;
-            if (identity is WindowsIdentity winIdentity)
+            if (OperatingSystem.IsWindows() && identity is WindowsIdentity winIdentity)
             {
-#pragma warning disable CA1416 // Validate platform compatibility
                 user = new WindowsPrincipal(winIdentity);
-#pragma warning restore CA1416 // Validate platform compatibility
                 Response.RegisterForDispose(winIdentity);
             }
             else
